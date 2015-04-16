444ceab8d07ae18bd62f0eb2db20a1aa30df95f8
romuald <romuald.baldy@gmail.com>
2015-04-16 10:20:52 +0200

    Premier commit: initialisation

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Projet petit-git</title>
+    </head>
+    <body>
+        <h1>Projet petit-git</h1>
+        <?<php></php>
+        // put your code here
+        ?>
+    </body>
+</html>
